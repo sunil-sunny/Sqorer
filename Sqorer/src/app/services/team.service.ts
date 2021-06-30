fbe63bbc7b72b38bea95613312ecd504d842d559
sunil-sunny <sunilsunny183@gmail.com>
2021-06-30 10:14:50 -0300

    Minor change in team service

--- Sqorer/src/app/services/team.service.ts
+++ Sqorer/src/app/services/team.service.ts
@@ -2,7 +2,7 @@
 import { Observable } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { environment } from '../../environments/environment';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'

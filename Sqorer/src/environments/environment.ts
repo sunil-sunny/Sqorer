c6f0fde1f1dcbfe2e7b09ac4dd7ff8cc55277895
sunil-sunny <sunilsunny183@gmail.com>
2021-07-12 11:11:33 -0300

    environment changes

--- Sqorer/src/environments/environment.ts
+++ Sqorer/src/environments/environment.ts
@@ -4,17 +4,8 @@
 
 export const environment = {
   production: false,
-  firebaseConfig : {
-    apiKey: 'AIzaSyDMOetmzd1U6yFLFdeJmfeNmhAqV76lOrI',
-    authDomain: 'sqorer-d8e20.firebaseapp.com',
-    projectId: 'sqorer-d8e20',
-    storageBucket: 'sqorer-d8e20.appspot.com',
-    messagingSenderId: '354980587941',
-    appId: '1:354980587941:web:b8a1074c63353629a1a04c',
-    measurementId: 'G-ZD0ZH2RRE1'
-  },
-  serverUrl: 'http://localhost:8082/api/' ,
-  //serverUrl: 'http://test-service.sqorer.com/api/'
+  //serverUrl: 'http://localhost:8082/api/' ,
+  serverUrl: 'http://test-service.sqorer.com/api/'
 };
 
 /*
